8f98da2e30a93cbc836b02adbfce99cf9d1c1254
=AnhHoangNgo <=ngoanh1292000@gmail.com>
2019-08-08 16:12:57 +0700

    Update exercise06.js

--- fundamentals/session01/homework/Exercise06/exercise06.js
+++ fundamentals/session01/homework/Exercise06/exercise06.js
@@ -1,18 +1,19 @@
 let a=Number(prompt("Enter a")), b=Number(prompt("Enter b")), c=Number(prompt("Enter c"));
-if(a==0){
-    if(b==0 && c==0){
+if(a===0){
+    if(b===0 && c===0){
         console.log("The equation's alway true ");}
-        else if(b==0 && c!=0){ 
+        else if(b===0 && c!=0){ 
             console.log("The equation hasn't root");}
     else {
         console.log("The equation has a root");
         console.log( "r ="+c/b)}}
 else{
-    if(b*b-4*a*c > 0){
+    let denta = b*b-4*a*c;
+    if(denta > 0){
         console.log("The equation has two roots");
-        console.log("r1 = "+ (-b +Math.sqrt(b*b-4*a*c))/(2*a));
-        console.log("r2 = "+(-b -Math.sqrt(b*b-4*a*c))/(2*a));
-    }else if( b*b-4*a*c==0){
+        console.log("r1 = "+ (-b +Math.sqrt(denta))/(2*a));
+        console.log("r2 = "+(-b -Math.sqrt(denta))/(2*a));
+    }else if( denta===0){
         console.log("The equation has a root")
         console.log("r= "+-b/(2*a))}
     else {
