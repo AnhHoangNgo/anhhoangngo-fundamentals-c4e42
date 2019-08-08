127ef274c9fc7642cafda4cbc642e6b8939e0112
=AnhHoangNgo <=ngoanh1292000@gmail.com>
2019-08-08 15:47:41 +0700

    Update exercise03.js

--- fundamentals/session01/homework/Exercise03/exercise03.js
+++ fundamentals/session01/homework/Exercise03/exercise03.js
@@ -1,7 +1,7 @@
 let x = Math.random()*101;
 if(x<30){
     console.log("Rainy");}
-    else if(60>= x && x >=30){
+else if(60>= x ){
     console.log("Cloudy");}
-    else if(x>60){
+else{
     console.log("Sunny");}
