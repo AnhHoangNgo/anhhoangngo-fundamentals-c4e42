eee9ffda3ff03d9b14dc96f9eacaefdeeff80312
=AnhHoangNgo <=ngoanh1292000@gmail.com>
2019-09-08 09:23:35 +0700

    Create demo.js

---
+++ fundamentals/session10/HomeWork/demo.js
@@ -0,0 +1,32 @@
+const box = {
+    left:0,
+    top:0,
+    speed:10, 
+};
+const moveTo = function(){
+domObject('up').addEventListener('click',moveToUp);
+domObject('down').addEventListener('click',moveToDown);
+domObject('left').addEventListener('click',moveToLeft);
+domObject('right').addEventListener('click',moveToRight);
+}
+const domObject = function (id){
+    return document.getElementById(id);
+}
+const moveToUp = function(){
+    box.top -= box.speed;
+    domObject('box').style.top = `${box.top}px`;
+}
+const  moveToDown = function(){
+    box.top += box.speed;
+    domObject('box').style.top = `${box.top}px`;
+}
+const moveToLeft = function(){
+    box.left -= box.speed;
+    domObject('box').style.left = `${box.left}px`;
+    colorBorderBtn ('left');
+}
+const moveToRight = function(){
+    box.left += box.speed;
+    domObject('box').style.left = `${box.left}px`
+}
+moveTo();
