90eab313cbc3355315608a9e5b82200a732ad274
=AnhHoangNgo <=ngoanh1292000@gmail.com>
2019-09-20 05:31:05 +0700

    Create components.js

---
+++ fundamentals/FileNew/js/components.js
@@ -0,0 +1,26 @@
+const components = {};
+components.pageHome=`
+
+`;
+components.pageAbout=`
+<section class='header headerpageAbout'>
+    </section>
+    <section class="mainPage pageAbout">
+    </section>
+    <section class="mainPage pageAbout">
+    </section>
+    <section class="mainPage pageAbout">
+    </section>
+`;
+components.pageGallery=`
+<section class='header headerpageAbout'>
+    </section>
+    <section class="mainPage pageHome">
+    </section>
+    <section class="mainPage pageHome">
+    </section>
+    <section class="mainPage pageHome">
+    </section>
+`;
+components.pageContact=`
+`;
